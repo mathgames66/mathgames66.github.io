295d013918867e58eef1a3397497a19cf2a93736
rm <90115544+68r@users.noreply.github.com>
2022-03-23 19:22:01 -0700

    Add files via upload

---
+++ 905047eb-b3c0-41f9-b185-d7afcc2fea97/content/app-104591.games.s3.yandex.net/104591/zgp6tt4tnfwfmwn25tz54gk237f6tspn/register-sw.js
@@ -0,0 +1,1 @@
+"use strict";!function(){function e(e){console.warn("Failed to register service worker: ",e)}window.C3_RegisterSW=function(){if(navigator.serviceWorker)try{navigator.serviceWorker.register("sw.js",{scope:"./"}).then(function(e){console.log("Registered service worker on "+e.scope)}).catch(e)}catch(r){e(r)}}}();
