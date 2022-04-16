3065405833100934164f2df0bb7649ed6c5deb58
rm <90115544+68r@users.noreply.github.com>
2022-04-16 09:49:30 -0700

    LICENSE

---
+++ uvnostatic/client/dom/attr.js
@@ -0,0 +1,123 @@
+import EventEmitter from "../events.js";
+import HookEvent from "../hook.js";
+
+class AttrApi extends EventEmitter {
+    constructor(ctx) {
+        super();
+        this.ctx = ctx;
+        this.window = ctx.window;
+        this.Attr = this.window.Attr || {};
+        this.attrProto = this.Attr.prototype || {};
+        this.value = ctx.nativeMethods.getOwnPropertyDescriptor(this.attrProto, 'value');
+        this.name = ctx.nativeMethods.getOwnPropertyDescriptor(this.attrProto, 'name');
+        this.getNamedItem = this.attrProto.getNamedItem || null;
+        this.setNamedItem = this.attrProto.setNamedItem || null;
+        this.removeNamedItem = this.attrProto.removeNamedItem || null;
+        this.getNamedItemNS = this.attrProto.getNamedItemNS || null;
+        this.setNamedItemNS = this.attrProto.setNamedItemNS || null;
+        this.removeNamedItemNS = this.attrProto.removeNamedItemNS || null;
+        this.item = this.attrProto.item || null;
+    };
+    overrideNameValue() {
+        this.ctx.overrideDescriptor(this.attrProto, 'name', {
+            get: (target, that) => {
+                const event = new HookEvent({ value: target.call(that) }, target, that);
+                this.emit('name', event);
+
+                if (event.intercepted) return event.returnValue;
+                return event.data.value;
+            },
+        });
+        
+        this.ctx.overrideDescriptor(this.attrProto, 'value', {
+            get: (target, that) => {
+                const event = new HookEvent({ name: this.name.get.call(that), value: target.call(that) }, target, that);
+                this.emit('getValue', event);
+
+                if (event.intercepted) return event.returnValue;
+                return event.data.value;
+            },
+            set: (target, that, [ val ]) => {
+                const event = new HookEvent({ name: this.name.get.call(that), value: val }, target, that);
+                this.emit('setValue', event);
+
+                if (event.intercepted) return event.returnValue;
+                event.target.call(event.that, event.data.value);
+            }
+        });
+    };
+    overrideItemMethods() {
+        this.ctx.override(this.attrProto, 'getNamedItem', (target, that, args) => {
+            if (!args.length) return target.apply(that, args);
+            let [ name ] = args;
+
+            const event = new HookEvent({ name }, target, that);
+            this.emit('getNamedItem', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.name);
+        });
+        this.ctx.override(this.attrProto, 'setNamedItem', (target, that, args) => {
+            if (2 > args.length) return target.apply(that, args);
+            let [ name, value ] = args;
+
+            const event = new HookEvent({ name, value }, target, that);
+            this.emit('setNamedItem', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.name, event.data.value);
+        });
+        this.ctx.override(this.attrProto, 'removeNamedItem', (target, that, args) => {
+            if (!args.length) return target.apply(that, args);
+            let [ name ] = args;
+
+            const event = new HookEvent({ name }, target, that);
+            this.emit('removeNamedItem', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.name);
+        });
+        this.ctx.override(this.attrProto, 'item', (target, that, args) => {
+            if (!args.length) return target.apply(that, args);
+            let [ index ] = args;
+
+            const event = new HookEvent({ index }, target, that);
+            this.emit('item', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.name);
+        });
+        this.ctx.override(this.attrProto, 'getNamedItemNS', (target, that, args) => {
+            if (2 > args.length) return target.apply(that, args);
+            let [ namespace, localName ] = args;
+
+            const event = new HookEvent({ namespace, localName }, target, that);
+            this.emit('getNamedItemNS', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.namespace, event.data.localName);
+        });
+        this.ctx.override(this.attrProto, 'setNamedItemNS', (target, that, args) => {
+            if (!args.length) return target.apply(that, args);
+            let [ attr ] = args;
+
+            const event = new HookEvent({ attr }, target, that);
+            this.emit('setNamedItemNS', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.name);
+        });
+        this.ctx.override(this.attrProto, 'removeNamedItemNS', (target, that, args) => {
+            if (2 > args.length) return target.apply(that, args);
+            let [ namespace, localName ] = args;
+
+            const event = new HookEvent({ namespace, localName }, target, that);
+            this.emit('removeNamedItemNS', event);
+
+            if (event.intercepted) return event.returnValue;
+            return event.target.call(event.that, event.data.namespace, event.data.localName);
+        });
+    };
+};
+
+export default AttrApi;
--- uvnostatic/client/events.js
+++ uvnostatic/client/events.js
@@ -1,319 +1,25 @@
-importScripts('/uv/uv.bundle.js');
-importScripts('/uv/uv.config.js');
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-class UVServiceWorker extends EventEmitter {     
-    constructor(config = __uv$config) {
-        super();
-        if (!config.bare) config.bare = '/bare/';
-        this.addresses = typeof config.bare === 'string' ? [ new URL(config.bare, location) ] : config.bare.map(str => new URL(str, location));
-        this.headers = {
-            csp: [
-                'cross-origin-embedder-policy',
-                'cross-origin-opener-policy',
-                'cross-origin-resource-policy',
-                'content-security-policy',
-                'content-security-policy-report-only',
-                'expect-ct',
-                'feature-policy',
-                'origin-isolation',
-                'strict-transport-security',
-                'upgrade-insecure-requests',
-                'x-content-type-options',
-                'x-download-options',
-                'x-frame-options',
-                'x-permitted-cross-domain-policies',
-                'x-powered-by',
-                'x-xss-protection',
-            ],
-            forward: [
-                'accept-encoding', 
-                'connection',
-                'content-length',
-            ],
-        };
-        this.method = {
-            empty: [
-                'GET',
-                'HEAD'
-            ]
-        };
-        this.statusCode = {
-            empty: [ 
-                204,
-                304,
-            ],
-        };  
-        this.config = config;
-        this.browser = Ultraviolet.Bowser.getParser(self.navigator.userAgent).getBrowserName();
-
-        if (this.browser === 'Firefox') {
-            this.headers.forward.push('user-agent');
-            this.headers.forward.push('content-type');
-        };
-    };
-    async fetch({ request }) {
-        if (!request.url.startsWith(location.origin + (this.config.prefix || '/service/'))) {
-            return fetch(request);
-        };
-        try {
-
-            const ultraviolet = new Ultraviolet(this.config);
-
-            if (typeof this.config.construct === 'function') {
-                this.config.construct(ultraviolet, 'service');
-            };
-
-            const db = await ultraviolet.cookie.db();
-
-            ultraviolet.meta.origin = location.origin;
-            ultraviolet.meta.base = ultraviolet.meta.url = new URL(ultraviolet.sourceUrl(request.url));
-
-            const requestCtx = new RequestContext(
-                request, 
-                this, 
-                ultraviolet, 
-                !this.method.empty.includes(request.method.toUpperCase()) ? await request.blob() : null
-            );
-
-            if (ultraviolet.meta.url.protocol === 'blob:') {
-                requestCtx.blob = true;
-                requestCtx.base = requestCtx.url = new URL(requestCtx.url.pathname);
-            };
-
-            if (request.referrer && request.referrer.startsWith(location.origin)) {
-                const referer = new URL(ultraviolet.sourceUrl(request.referrer));
-
-                if (requestCtx.headers.origin || ultraviolet.meta.url.origin !== referer.origin && request.mode === 'cors') {
-                    requestCtx.headers.origin = referer.origin;
-                };
-
-                requestCtx.headers.referer = referer.href;
-            };
-
-            const cookies = await ultraviolet.cookie.getCookies(db) || [];
-            const cookieStr = ultraviolet.cookie.serialize(cookies, ultraviolet.meta, false);
-
-            if (this.browser === 'Firefox' && !(request.destination === 'iframe' || request.destination === 'document')) {
-                requestCtx.forward.shift();
-            };
-
-            if (cookieStr) requestCtx.headers.cookie = cookieStr;
-            requestCtx.headers.Host = requestCtx.url.host;
-
-
-            const reqEvent = new HookEvent(requestCtx, null, null);
-            this.emit('request', reqEvent);
-
-            if (reqEvent.intercepted) return reqEvent.returnValue;
-
-            const response = await fetch(requestCtx.send);
-
-            if (response.status === 500) {
-                return Promise.reject('');
-            };
-
-            const responseCtx = new ResponseContext(requestCtx, response, this);
-            const resEvent = new HookEvent(responseCtx, null, null);
-
-            this.emit('beforemod', resEvent);
-            if (resEvent.intercepted) return resEvent.returnValue;
-
-            for (const name of this.headers.csp) {
-                if (responseCtx.headers[name]) delete responseCtx.headers[name];
-            }; 
-            
-            if (responseCtx.headers.location) {
-                responseCtx.headers.location = ultraviolet.rewriteUrl(responseCtx.headers.location);
-            };
-
-            if (responseCtx.headers['set-cookie']) {
-                Promise.resolve(ultraviolet.cookie.setCookies(responseCtx.headers['set-cookie'], db, ultraviolet.meta)).then(() => {
-                    self.clients.matchAll().then(function (clients){
-                        clients.forEach(function(client){
-                            client.postMessage({
-                                msg: 'updateCookies',
-                                url: ultraviolet.meta.url.href,
-                            });
-                        });
-                    });
-                });
-                delete responseCtx.headers['set-cookie'];
-            };
-
-            if (responseCtx.body) {
-                switch(request.destination) {
-                    case 'script':
-                    case 'worker':
-                        responseCtx.body = `if (!self.__uv && self.importScripts) importScripts('${__uv$config.bundle}', '${__uv$config.config}', '${__uv$config.handler}');\n`;
-                        responseCtx.body += ultraviolet.js.rewrite(
-                            await response.text()
-                        );
-                        break;
-                    case 'style':
-                        responseCtx.body = ultraviolet.rewriteCSS(
-                            await response.text()
-                        ); 
-                        break;
-                case 'iframe':
-                case 'document':
-                        if (isHtml(ultraviolet.meta.url, (responseCtx.headers['content-type'] || ''))) {
-                            responseCtx.body = ultraviolet.rewriteHtml(
-                                await response.text(), 
-                                { 
-                                    document: true ,
-                                    injectHead: ultraviolet.createHtmlInject(
-                                        this.config.handler, 
-                                        this.config.bundle, 
-                                        this.config.config,
-                                        ultraviolet.cookie.serialize(cookies, ultraviolet.meta, true), 
-                                        request.referrer
-                                    )
-                                }
-                            );      
-                        };
-                };
-            };
-
-            if (requestCtx.headers.accept === 'text/event-stream') {
-                responseCtx.headers['content-type'] = 'text/event-stream';
-            };
-
-            this.emit('response', resEvent);
-            if (resEvent.intercepted) return resEvent.returnValue;
-
-            return new Response(responseCtx.body, {
-                headers: responseCtx.headers,
-                status: responseCtx.status,
-                statusText: responseCtx.statusText,
-            });
-
-        } catch(err) {
-            return new Response(err.toString(), {
-                status: 500,
-            });
-        };
-    };
-    getBarerResponse(response) {
-        const headers = {};
-        const raw = JSON.parse(response.headers.get('x-bare-headers'));
-
-        for (const key in raw) {
-            headers[key.toLowerCase()] = raw[key];
-        };
-
-        return {
-            headers,
-            status: +response.headers.get('x-bare-status'),
-            statusText: response.headers.get('x-bare-status-text'),
-            body: !this.statusCode.empty.includes(+response.headers.get('x-bare-status')) ? response.body : null,
-        };
-    };
-    get address() {
-        return this.addresses[Math.floor(Math.random() * this.addresses.length)];
-    };
-    static Ultraviolet = Ultraviolet;
-};
-
-self.UVServiceWorker = UVServiceWorker;
-
-
-class ResponseContext {
-    constructor(request, response, worker) {
-        const { headers, status, statusText, body } = !request.blob ? worker.getBarerResponse(response) : {
-            status: response.status, 
-            statusText: response.statusText,
-            headers: Object.fromEntries([...response.headers.entries()]),
-            body: response.body,
-        };
-        this.request = request;
-        this.raw = response;
-        this.ultraviolet = request.ultraviolet;
-        this.headers = headers;
-        this.status = status;
-        this.statusText = statusText;
-        this.body = body;
-    };
-    get url() {
-        return this.request.url;
-    }
-    get base() {
-        return this.request.base;
-    };
-    set base(val) {
-        this.request.base = val;
-    };
-};
-
-class RequestContext {
-    constructor(request, worker, ultraviolet, body = null) {
-        this.ultraviolet = ultraviolet;
-        this.request = request;
-        this.headers = Object.fromEntries([...request.headers.entries()]);
-        this.method = request.method;
-        this.forward = [...worker.headers.forward];
-        this.address = worker.address;
-        this.body = body || null;
-        this.redirect = request.redirect;
-        this.credentials = 'omit';
-        this.mode = request.mode === 'cors' ? request.mode : 'same-origin';
-        this.blob = false;
-    };
-    get send() {
-        return new Request((!this.blob ? this.address.href + 'v1/' : 'blob:' + location.origin + this.url.pathname), {
-            method: this.method,
-            headers: {
-                'x-bare-protocol': this.url.protocol,
-                'x-bare-host': this.url.hostname,
-                'x-bare-path': this.url.pathname + this.url.search,
-                'x-bare-port': this.url.port || (this.url.protocol === 'https:' ? '443' : '80'),
-                'x-bare-headers': JSON.stringify(this.headers),
-                'x-bare-forward-headers': JSON.stringify(this.forward),
-            },
-            redirect: this.redirect,
-            credentials: this.credentials,
-            mode: location.origin !== this.address.origin ? 'cors' : this.mode,
-            body: this.body
-        });
-    };
-    get url() {
-        return this.ultraviolet.meta.url;
-    };
-    set url(val) {
-        this.ultraviolet.meta.url = val;
-    };
-    get base() {
-        return this.ultraviolet.meta.base;
-    };
-    set base(val) {
-        this.ultraviolet.meta.base = val;
-    };
-}
-
-function isHtml(url, contentType = '') {
-    return (Ultraviolet.mime.contentType((contentType  || url.pathname)) || 'text/html').split(';')[0] === 'text/html';
-};
-
-class HookEvent {
-    #intercepted;
-    #returnValue;
-    constructor(data = {}, target = null, that = null) {
-        this.#intercepted = false;
-        this.#returnValue = null;
-        this.data = data;
-        this.target = target;
-        this.that = that;
-    };
-    get intercepted() {
-        return this.#intercepted;
-    };
-    get returnValue() {
-        return this.#returnValue;
-    };
-    respondWith(input) {
-        this.#returnValue = input;
-        this.#intercepted = true;
-    };
-};  
+'use strict';
 
 var R = typeof Reflect === 'object' ? Reflect : null
 var ReflectApply = R && typeof R.apply === 'function'
@@ -347,6 +53,8 @@ var NumberIsNaN = Number.isNaN || function NumberIsNaN(value) {
 function EventEmitter() {
   EventEmitter.init.call(this);
 }
+
+export default EventEmitter;
 
 // Backwards-compat with node 0.10.x
 EventEmitter.EventEmitter = EventEmitter;
