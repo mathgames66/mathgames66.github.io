77555491391624878b937c611a9f9fa60fe5ec7d
rm <90115544+68r@users.noreply.github.com>
2022-05-09 16:35:34 -0700

    Add files via upload

---
+++ uy/content/19251.cache.armorgames.com/files/games/yarn-untangled-19251/src/settings.18b6a.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/integration/splash.fire",orientation:"",jsList:[],bundleVers:{internal:"4aef9",resources:"c926e",main:"f6dfa"}};
