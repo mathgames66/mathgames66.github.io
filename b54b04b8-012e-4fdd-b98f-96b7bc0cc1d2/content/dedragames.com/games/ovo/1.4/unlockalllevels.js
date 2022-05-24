794d551624c15be4e376e06acb56a0e7a72d53a6
rm <90115544+68r@users.noreply.github.com>
2022-05-23 20:34:12 -0700

    Add files via upload

---
+++ b54b04b8-012e-4fdd-b98f-96b7bc0cc1d2/content/dedragames.com/games/ovo/1.4/unlockalllevels.js
@@ -0,0 +1,11 @@
+function unlockAllLevels(){
+	c2_callFunction("unlockAllLevels");
+}
+
+function execCode(code){
+	c2_callFunction("execCode", [code]);
+}
+
+function dumpSave() {
+	c2_callFunction("dumpSave");
+}
