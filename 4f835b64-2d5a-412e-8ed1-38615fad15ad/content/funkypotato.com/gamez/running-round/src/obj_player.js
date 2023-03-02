df30806e7f1c3079de00e89f539dc077876bc1cc
you may know me as "deleted" / "rm" / "rmwastaken <90115544+68r@users.noreply.github.com>
2023-03-02 07:03:59 -0800

    Add files via upload

---
+++ 4f835b64-2d5a-412e-8ed1-38615fad15ad/content/funkypotato.com/gamez/running-round/src/obj_player.js
@@ -0,0 +1,12 @@
+var gravity=0.5;createObject("obj_player",{init:function(){this.width=8;this.height=24;this.vspeed=0;this.flag=true;this.dr=0;},loop:function(){ctx.fillStyle=COLOR_PLAYER;if(this.y<canvas.height-100){var dd=-1+this.dr*2;dd=-dd/2;drawTransformedRectangle(this.x-this.width/2+dd/2,this.y-this.height/2+dd,this.width-dd/2*2,this.height-dd,true);}
+if(this.flag){if(this.dr<1){this.dr+=0.04;}else{this.flag=false;}}else{if(this.dr>0){this.dr-=0.04;}else{this.flag=true;}}
+this.dr=Math.max(0,Math.min(this.dr,1));var side=16
+if(keysDown[" "]||keysDown["ArrowUp"]||buttonsDown[0]){if(this.vspeed<0){this.vspeed-=0.25;}
+if(this.vspeed>=0&&collidesWall(this,this.x,this.y+this.vspeed+1)){this.vspeed=-6;playSound("snd_jump");this.dr=1;this.flag=false;}}
+var wall=collidesWall(this,this.x,this.y+this.vspeed);if(wall!==null){if(this.vspeed>=0){this.y=wall.y-this.height/2;}else{this.y=wall.y+wall.height+this.height/2;}
+this.vspeed=0;}
+this.y+=this.vspeed;this.vspeed+=gravity;if(this.y>canvas.height+64||collidesWall(this,this.x+1,this.y-this.vspeed)){gameOver(this);this.y=0;this.vspeed=0;}
+var spk=collidesSpike(this,this.x,this.y);if(spk!==null&&(spikeState===2||spikeState===1&&!spk.hard)){gameOver(this);this.y=0;this.vspeed=0;}}});function gameOver(player){playSound("snd_die");stopMusic();running=false;flashFade=2;var p=transformRectangle(player.x-player.width/2,player.y-player.height/2,player.width,player.height,true);var xx=(p.x1+p.x2)/2;var yy=(p.y1+p.y3)/2;destroyInstance(player);for(var i=0;i<30;i++){createInstance("obj_splash",xx,yy,-1000);}
+died=true;shaking=true;setTimeout(function(){shaking=false;},500);setTimeout(leaveGame,2000);}
+function collidesWall(instance,x,y){var collides=null;var w=instance.width;var h=instance.height;x-=w/2;y-=h/2;forInstances("obj_wall",function(wall){if(wall.x<=x+w&&x<wall.x+wall.width&&wall.y<=y+h&&y<wall.y+wall.height){collides=wall;}},instance);return collides;}
+function collidesSpike(instance,x,y){var collides=null;var w=instance.width;var h=instance.height;x-=w/2;y-=h/2;var dx=4;var dy=2;forInstances("obj_spike",function(spike){if(spike.x+dx<=x+w&&x<spike.x+spike.width-dx&&spike.y+dy<=y+h&&y<spike.y+spike.height-dy){collides=spike;}},instance);return collides;}
---
+++ 4f835b64-2d5a-412e-8ed1-38615fad15ad/content/funkypotato.com/gamez/running-round/src/transform.js
@@ -0,0 +1,6 @@
+var centerX=canvas.width/2;var centerY=canvas.height;var angleLength=340;var radiusRatio=1.4-0.2
+var circX=canvas.width/2;var circY=375+50+50;var circYStart=circY;if(warpType===2){circY=canvas.height-circY;}
+function pointDirection(x1,y1,x2,y2){return Math.atan2(y1-y2,x2-x1);}
+function pointDistance(x1,y1,x2,y2){return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));}
+function transformPoint(x,y){if(warpType===0){return{x:x,y:y,};}else if(warpType===1){var angle=(centerX-x)/angleLength*Math.PI+Math.PI/2;var radius=(centerY-y)/radiusRatio;return{x:circX+Math.cos(angle)*radius,y:circY-Math.sin(angle)*radius,};}else if(warpType===2){var cy=canvas.height-centerY;var angle=(x-centerX)/angleLength*Math.PI-Math.PI/2*3;var radius=(cy-y)/radiusRatio;return{x:circX+Math.cos(angle)*radius,y:circY-Math.sin(angle)*radius,};}}
+function transformRectangle(x1,y1,width,height){var x2=x1+width;var y2=y1+height;var point1=transformPoint(x1,y1);var point2=transformPoint(x2,y1);var point3=transformPoint(x2,y2);var point4=transformPoint(x1,y2);return{x1:point1.x,y1:point1.y,x2:point2.x,y2:point2.y,x3:point3.x,y3:point3.y,x4:point4.x,y4:point4.y,};}
