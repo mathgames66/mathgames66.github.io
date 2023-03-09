aaadc938040266cf819d376c51e95bd2c764e65d
you may know me as "deleted" / "rm" / "rmwastaken <90115544+68r@users.noreply.github.com>
2023-03-09 06:44:33 -0800

    Add files via upload

---
+++ d036e682-c7fe-4a97-a4a6-efb55ce6cff0/content/www.spiele-umsonst.de/azad/downloads/html5games/puzzle/jellydoods/js/game.api.js
@@ -0,0 +1,14 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+// file game.api.js
+// Copyright (c) 2015 Frédéric J. Rézeau. All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+
+(function (namespace, undefined) {
+    "use strict";
+
+    namespace.isDesktop = function () {
+        return categorizr.isDesktop ? true : false;
+    };
+
+})(window.OkijinAPI = window.OkijinAPI || {});
+
